2c1810d872cf9060cdee5ce5d13d85d23f7c605e
pullerize <111420360+pullerize@users.noreply.github.com>
2025-07-16 17:23:40 +0500

    Import prices data

--- DOOR_CALCULATOR/assets/scripts/script.js
+++ DOOR_CALCULATOR/assets/scripts/script.js
@@ -14,6 +14,7 @@
 */
 
 import { componentNames } from '../data/componentNames.js';
+import { prices } from '../data/prices.js';
 import { loadSaved, saveIfNeeded } from '../utils/localStorage.js';
 import { formatPhone } from '../utils/phone.js';
 
